daf193af16c77fe6f40cfaa51c98db3c8f5913fd
柯慕灵 <56398475+1985312383@users.noreply.github.com>
2026-01-05 14:58:48 +0800

    Add image viewer plugin and enhance docs with images
    Integrated vitepress-plugin-image-viewer for enhanced image interaction in documentation. Added custom CSS for improved layout and image display. Updated the first concepts tutorial with detailed content and illustrative images. Added several images to the public assets for use in documentation.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,19 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import './custom.css'
+import type { Theme } from 'vitepress'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import { useRoute } from 'vitepress';
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+    extends: DefaultTheme,
+    enhanceApp({ app }) {
+        // 注册全局组件（可选）
+        app.component('imageViewer', imageViewer);
+    },
+    setup() {
+        const route = useRoute();
+        // 启用插件
+        imageViewer(route);
+    },
 } satisfies Theme
